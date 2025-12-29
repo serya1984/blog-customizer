e6c8a0973d009677c51c29c03521a15c93988716
Sergei.Ostapenko <serya1684@mail.ru>
2025-12-29 20:48:50 +0600

    second commit

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import plane from 'src/images/plane.png';
 import { Text } from 'src/ui/text';
-
 import styles from './Article.module.scss';
 
 export const Article = () => {
